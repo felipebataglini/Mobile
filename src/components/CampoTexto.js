f3a81dff8fb63b8c01b8f610c82a3ce5f0ef4bc7
Andrei <andrei.rezende@hotmail.com>
2025-10-04 05:46:16 -0300

    feat: add CampoTexto component

---
+++ src/components/CampoTexto.js
@@ -0,0 +1,26 @@
+import { View, StyleSheet, Text } from "react-native";
+import { TextInput } from "react-native-paper";
+
+const CampoTexto = (props) => {
+
+    return (
+            <View style={estilos.container}>
+                <Text style={estilos.tituloCampo}>{props.texto}</Text>
+                <TextInput value={props.valor} onChangeText={props.funcao} keyboardType={props.tipoTeclado} secureTextEntry={props.secure}/>
+            </View>
+    )
+}
+
+const estilos = StyleSheet.create({
+    container: {
+        width: '100%',
+    },
+    tituloCampo: {
+        fontSize: 16,
+        marginBottom: 2,
+        color: '#FFFFFF',
+        fontFamily: 'AveriaLibre-Regular'
+    }
+})
+
+export default CampoTexto
