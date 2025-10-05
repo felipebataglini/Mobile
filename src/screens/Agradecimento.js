593b8bd8de620e453b412769540e75e40f468c65
felipebataglini <bataglini.felipe@gmail.com>
2025-10-04 23:31:59 -0300

    feat: Implement agradecimento screen UI

--- src/screens/Agradecimento.js
+++ src/screens/Agradecimento.js
@@ -1,11 +1,50 @@
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { PaperProvider, MD3LightTheme as DefaultTheme } from 'react-native-paper';
 
-const Agradecimento = () => {
+// Tema consistente com as outras telas
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#FFFFFF',
+        secondary: 'black'
+    }
+};
+
+const Agradecimento = (props) => {
+
+    // Função para navegar para a tela 'Ações Pesquisa' ao pressionar
+    const navegarParaAcoes = () => {
+        props.navigation.navigate('AcoesPesquisa');
+    };
+
     return (
-        <View>
-            <Text>Tela Agradecimento</Text>
-        </View>
-    )
-}
+        <PaperProvider theme={theme}>
+            <TouchableOpacity style={estilos.container} onPress={navegarParaAcoes}>
+                <Text style={estilos.texto}>
+                    {'Obrigado por participar da pesquisa!\nAguardamos você no próximo ano!'}
+                </Text>
+            </TouchableOpacity>
+        </PaperProvider>
+    );
+};
 
-export default Agradecimento
+const estilos = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#372775', //
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    texto: {
+        color: '#FFFFFF', //
+        fontFamily: 'Averia Libre', //
+        fontWeight: '400', //
+        fontSize: 48, //
+        textAlign: 'center', //
+        lineHeight: 48, //
+    }
+});
+
+export default Agradecimento;
