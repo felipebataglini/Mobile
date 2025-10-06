0b9d95341f760d7af15a18c7c3110fcd7db621c5
Andrei <andrei.rezende@hotmail.com>
2025-10-06 17:01:57 -0300

    feat: Implement Relatorio screen UI

--- src/screens/Relatorio.js
+++ src/screens/Relatorio.js
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 
 const Relatorio = () => {
     return (
-        <View>
-            <Text>Tela Relatorio</Text>
+        <View style={estilos.container}>
+            <Image source={require("../../assets/images/graficoRelatorio.png")} style={estilos.imagem}/>
         </View>
     )
 }
+
+const estilos = StyleSheet.create({
+    container: {
+        backgroundColor: '#372775',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1
+    },
+    imagem: {
+        width: "70%",
+        height: "85%"
+    }
+})
 
 export default Relatorio
