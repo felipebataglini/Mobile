127d26c0ddf9afcd4583f15fc25e5261231a3923
Andrei <andrei.rezende@hotmail.com>
2025-10-06 17:05:15 -0300

    feat: Implement ModificarPesquisa screen UI

--- src/screens/ModificarPesquisa.js
+++ src/screens/ModificarPesquisa.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, StyleSheet, Text, Image } from 'react-native'
+import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
 import Botao from '../components/Botao'
 import { TextInput, PaperProvider } from 'react-native-paper'
+import IconePopUp from "../components/IconePopUp"
 
 const theme = {
     colors: {
@@ -27,23 +28,28 @@ const ModificarPesquisa = (props) => {
     return (
         <PaperProvider theme={theme}>
             <View style={estilos.container}>
-                <View style={{ width: '80%', height: '90%', justifyContent: 'space-between', margin: 'auto'}}>
+                <View style={{ width: '60%', height: '95%', justifyContent: 'space-between', margin: 'auto'}}>
                     <View>
                         <Text style={estilos.tituloCampo}>Nome</Text>
-                        <TextInput mode="outlined" theme={theme} value={nome} onChangeText={setNome} textColor="#3F92C5" style={{ height: 50, justifyContent: 'center', backgroundColor: '#FFFFFF', fontSize: 14}} />
+                        <TextInput mode="outlined" theme={theme} value={nome} onChangeText={setNome} textColor="#3F92C5" style={estilos.input} />
                     </View>
                     <View>
                         <Text style={estilos.tituloCampo}>Data</Text>
-                        <TextInput mode="outlined" theme={theme} value={data} onChangeText={setData} textColor="#3F92C5" style={{ height: 50, justifyContent: 'center', backgroundColor: '#FFFFFF', fontSize: 14}} 
+                        <TextInput mode="outlined" theme={theme} value={data} onChangeText={setData} textColor="#3F92C5" style={estilos.input} 
                             right={<TextInput.Icon icon="calendar-month" color="#939393" />}
                         />
                     </View>
-                    <View>
+                    <View style={{ marginBottom: 20 }}>
                         <Text style={estilos.tituloCampo}>Imagem</Text>
-                        <Image source={require("../../assets/images/carnaval.png")} style={{height: 70, width: "70%"}}/>
+                        <Image source={require("../../assets/images/carnaval.png")} style={{height: 50, width: 230}}/>
                     </View>
-                    <View>
-                        <Botao texto="SALVAR" funcao={salvar} cor='#37BD6D'/> 
+                    <View style={{ flexDirection:'row'}}>
+                        <View style={{ width: 383 }}>
+                            <Botao texto="SALVAR" funcao={salvar} cor='#37BD6D' altura={25}/> 
+                        </View>
+                        <View style={{ marginLeft: 75}}>
+                            <IconePopUp props={props} />
+                        </View>
                     </View>
                 </View>
             </View>
@@ -58,10 +64,16 @@ const estilos = StyleSheet.create({
         flex: 1
     },
     tituloCampo: {
-        fontSize: 16,
+        fontSize: 14,
         marginBottom: 2,
         color: '#FFFFFF',
         fontFamily: 'AveriaLibre-Regular'
+    },
+    input: { 
+        height: 25,
+        justifyContent: 'center',
+        backgroundColor: '#FFFFFF',
+        fontSize: 14
     }
 })
 
